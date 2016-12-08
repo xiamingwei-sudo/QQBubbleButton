d3090dafa5c68733f4193ebe73a2e7f586ea50f9
夏明伟 <xiamingwei@xiamingwei-NB.local>
2016-12-08 21:17:10 +0800

    Initial Commit

---
+++ QQBubbleButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQBubbleButton
+//
+//  Created by 夏明伟 on 2016/12/8.
+//  Copyright © 2016年 夏明伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
